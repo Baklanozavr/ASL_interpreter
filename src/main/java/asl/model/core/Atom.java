1d02a5fc28e916134f9b39aeec3e825848aa76bc
Aleksandr <abaklan88@gmail.com>
2021-04-14 21:21:14 +0700

    equals and hashCode for atoms

--- src/main/java/asl/model/core/Atom.java
+++ src/main/java/asl/model/core/Atom.java
@@ -7,12 +7,14 @@ import org.jetbrains.annotations.NotNull;
  * Базовый тип для элементов, которые не могут иметь атрибутов
  */
 public abstract class Atom<T> extends ASLObject {
+    @NotNull
     protected final T value;
 
-    protected Atom(T value) {
+    protected Atom(@NotNull T value) {
         this.value = value;
     }
 
+    @NotNull
     public T value() {
         return value;
     }
@@ -25,5 +27,18 @@ public abstract class Atom<T> extends ASLObject {
     @Override
     public String toString() {
         return String.valueOf(value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Atom<?> atom = (Atom<?>) o;
+        return value.equals(atom.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
     }
 }
